82842d653c0a7e7c266319f5a9f20e80f2bc6a37
Volodymyr Mihdal <volodymyr.migdal@gmail.com>
2025-04-02 17:13:19 +0200

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,22 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    sourcemap: true,
-  },
+  base: "/sketchy-similarity/",
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
+  // plugins: [
+  //   react(),
+  //   mode === 'development' &&
+  //   componentTagger(),
+  // ].filter(Boolean),
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
+  // build: {
+  //   sourcemap: true,
+  // },
 }));
