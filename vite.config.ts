a1040340ba1816751473424f28254f3a8c193210
Volodymyr Mihdal <volodymyr.migdal@gmail.com>
2025-04-02 17:14:09 +0200

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,20 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/sketchy-similarity/",
-  // server: {
-  //   host: "::",
-  //   port: 8080,
-  // },
-  // plugins: [
-  //   react(),
-  //   mode === 'development' &&
-  //   componentTagger(),
-  // ].filter(Boolean),
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // build: {
   //   sourcemap: true,
   // },
